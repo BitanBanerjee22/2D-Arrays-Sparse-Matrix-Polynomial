34d4f3c66073f1d86b9c6cd5be03a7dfe9e8f9f4
BitanBanerjee22 <bitanbanerjee01@gmail.com>
2025-10-02 10:27:55 +0530

    Update Rotate matrix by 90 degrees (clockwise).py

--- Rotate matrix by 90 degrees (clockwise).py
+++ Rotate matrix by 90 degrees (clockwise).py
@@ -1,24 +1,13 @@
-#include <stdio.h>
+def rotate90(matrix):
+    n = len(matrix)
+    # transpose
+    for i in range(n):
+        for j in range(i,n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    # reverse each row
+    for row in matrix:
+        row.reverse()
 
-void rotate90(int n, int mat[n][n]) {
-    for (int i = 0; i < n; i++)
-        for (int j = i; j < n; j++) {
-            int tmp = mat[i][j];
-            mat[i][j] = mat[j][i];
-            mat[j][i] = tmp;
-        }
-    for (int i = 0; i < n; i++) {
-        for (int j = 0, k = n-1; j < k; j++, k--) {
-            int tmp = mat[i][j];
-            mat[i][j] = mat[i][k];
-            mat[i][k] = tmp;
-        }
-    }
-}
-
-int main() {
-    int mat[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-    rotate90(3,mat);
-    for(int i=0;i<3;i++){ for(int j=0;j<3;j++) printf("%d ", mat[i][j]); printf("\n"); }
-    return 0;
-}
+mat = [[1,2,3],[4,5,6],[7,8,9]]
+rotate90(mat)
+for r in mat: print(r)
