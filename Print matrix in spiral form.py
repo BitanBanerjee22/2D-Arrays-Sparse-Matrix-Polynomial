138fc0e15389e3409d38cdfe9227e148b6d7a067
BitanBanerjee22 <bitanbanerjee01@gmail.com>
2025-10-02 10:29:23 +0530

    Update Print matrix in spiral form.py

--- Print matrix in spiral form.py
+++ Print matrix in spiral form.py
@@ -1,25 +1,21 @@
-#include <stdio.h>
+def spiral_order(matrix):
+    res = []
+    top, bottom, left, right = 0, len(matrix)-1, 0, len(matrix[0])-1
+    while top <= bottom and left <= right:
+        for i in range(left, right+1):
+            res.append(matrix[top][i])
+        top += 1
+        for i in range(top, bottom+1):
+            res.append(matrix[i][right])
+        right -= 1
+        if top <= bottom:
+            for i in range(right, left-1, -1):
+                res.append(matrix[bottom][i])
+            bottom -= 1
+        if left <= right:
+            for i in range(bottom, top-1, -1):
+                res.append(matrix[i][left])
+            left += 1
+    return res
 
-void spiralPrint(int m, int n, int arr[m][n]) {
-    int top = 0, bottom = m - 1, left = 0, right = n - 1;
-    while (top <= bottom && left <= right) {
-        for (int i = left; i <= right; i++) printf("%d ", arr[top][i]);
-        top++;
-        for (int i = top; i <= bottom; i++) printf("%d ", arr[i][right]);
-        right--;
-        if (top <= bottom) {
-            for (int i = right; i >= left; i--) printf("%d ", arr[bottom][i]);
-            bottom--;
-        }
-        if (left <= right) {
-            for (int i = bottom; i >= top; i--) printf("%d ", arr[i][left]);
-            left++;
-        }
-    }
-}
-
-int main() {
-    int mat[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-    spiralPrint(3,3,mat);
-    return 0;
-}
+print(spiral_order([[1,2,3],[4,5,6],[7,8,9]]))
