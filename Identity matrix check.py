3e16616d39811ac57c753e962a96ec20cf92aaa7
BitanBanerjee22 <bitanbanerjee01@gmail.com>
2025-10-02 10:41:12 +0530

    Create Identity matrix check.py

---
+++ Identity matrix check.py
@@ -0,0 +1,9 @@
+def is_identity(matrix):
+    n=len(matrix)
+    for i in range(n):
+        for j in range(n):
+            if (i==j and matrix[i][j]!=1) or (i!=j and matrix[i][j]!=0):
+                return False
+    return True
+
+print(is_identity([[1,0,0],[0,1,0],[0,0,1]]))
